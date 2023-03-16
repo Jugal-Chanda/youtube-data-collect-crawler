5367821c3051111ae0f41ad328ab2f88ff0c283c
jugal-chanda <jugalchanda7@gmail.com>
2023-03-16 16:12:03 +0600

    create new file for every new search and also check form video_ids.txt

--- crawler.py
+++ crawler.py
@@ -4,12 +4,11 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-
 # Set up the YouTube API client
 scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 api_service_name = "youtube"
 api_version = "v3"
-client_secrets_file = "client_secrect_youtube_api.json"
+client_secrets_file = "client_secret_970828352629-5ib4l9c39pea636m7158msv73s1jgho7.apps.googleusercontent.com.json"
 
 flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
     client_secrets_file, scopes)
@@ -20,11 +19,18 @@ youtube = googleapiclient.discovery.build(
 
 
 # Set up the search request parameters
-search_query = "car burglary theft"
+search_query = "ring camera video of a car break-in"
 max_results = 50
 next_page_token = ""
 videos = set()
 page_ids = []
+new_videos = set()
+with open("video_ids.txt") as f:
+    lines = f.readlines()
+    videos = set(lines)
+
+videos = {x.strip() for x in videos}
+
 
 while len(videos) < 2000:
     # Call the YouTube API with the search request parameters
@@ -38,11 +44,15 @@ while len(videos) < 2000:
     ).execute()
 
     # Check that all video IDs in the response are unique
-    new_video_ids = {search_result["id"]["videoId"] for search_result in search_response.get("items", [])}
+    new_video_ids = {search_result["id"]["videoId"]
+                     for search_result in search_response.get("items", [])}
+    for x in new_video_ids:
+        if x not in videos:
+            new_videos.add(x)
     videos.update(new_video_ids)
 
-    with open("video_ids.txt", "w") as f:
-        f.write("\n".join(videos))
+    # with open("video_ids.txt", "w") as f:
+    #     f.write("\n".join(videos))
 
     next_page_token = search_response.get("nextPageToken", "")
     page_ids.append(next_page_token)
@@ -57,3 +67,10 @@ while len(videos) < 2000:
         print(search_response.get("prevPageToken", ""))
         print("no page token")
         break
+
+
+new_file_name = search_query.split(" ")
+new_file_name = "_".join(new_file_name)+".txt"
+
+with open(new_file_name, "w") as f:
+    f.write("\n".join(new_videos))
